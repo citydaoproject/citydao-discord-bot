93d763b52dcdab429371b2b9bb8dafe51adc9bc0
Will Jaw <willjaw@wills-mbp.lan>
2022-11-16 16:15:37 -0800

    fixed @everyone role issue

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: "guildMemberUpdate",
 	async execute(oldMember, newMember) {
-		console.log(oldMember, newMember);
+		//console.log(oldMember, newMember);
     
     //var defaultHost = "https://forum.citydao.io/";
     
@@ -11,28 +11,45 @@ module.exports = {
     / newRoleName = Name of the changed Role
     */
     var newRole = newMember.roles.cache.difference(oldMember.roles.cache).last();
-    console.log(newRole);
     var newRoleId = newRole.id;
     var newRoleName = newRole.name; 
+		//console.log(newRole);
+		console.log(`Role ID: ${newRoleId}`);
+		console.log(`Role Name: ${newRoleName}`);
+
+		console.log(newMember.roles.cache);
+
+    /* 
+    / MemberNickName = Discord nickname of the current user (string)
+    / MemberId = Discord ID of the current user (snowflake)
+    / MemberName = Discord username (if no username then nickname) of the current user (string)
+    */ 
+    var MemberNickName = newMember.nickname;
+    var MemberId = newMember.id;
+    var MemberName = newMember.displayName;
+    console.log(`Nickname: ${MemberNickName}`);
+    console.log(`ID: ${MemberId}`);
+    console.log(`Name: ${MemberName}`);
 
     // check whether we added or removed roles
     newMemberSize = newMember.roles.cache.size;
     oldMemberSize = oldMember.roles.cache.size;
-    if(newMemberSize > oldMemberSize) {
+		console.log(`new size: ${newMemberSize}`);
+		console.log(`old size: ${oldMemberSize}`);
+		// needed to +1 for oldMemberSize due to @everyone role in discord
+    if(newMemberSize > oldMemberSize+1) {
       // add logic for added role
       console.log("role added");
     } else {
       // add logic for removed role
       console.log("role removed");
     };
-
-    
     // get user name (finish logic)
     // user = `${defaultHost}/admin/users`
 
     // function to compare roles
     //function diffRole(arr1, arr2) {
-    //  return [...diffRole(arr1, arr2), ...diffRole(arr2, arr1)];
+    //  return [...diff(arr1, arr2), ...diff(arr2, arr1)];
     //
     //  function diff(a, b) {
     //    return a.filter(item => b.indexOf(item) === -1);
